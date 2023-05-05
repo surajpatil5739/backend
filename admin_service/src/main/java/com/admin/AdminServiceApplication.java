5b8d7010e71648695e8058920072bf9688d938e6
Nilesh Bhutda <bhutdanilesh2517@gmail.com>
2023-05-05 11:27:11 +0530

    added API gateway'

--- admin_service/src/main/java/com/admin/AdminServiceApplication.java
+++ admin_service/src/main/java/com/admin/AdminServiceApplication.java
@@ -2,6 +2,9 @@ package com.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class AdminServiceApplication {
@@ -9,5 +12,11 @@ public class AdminServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AdminServiceApplication.class, args);
 	}
-
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+ 
 }
